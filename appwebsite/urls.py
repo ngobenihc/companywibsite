8ab6901d2a82386c5a5112d8a1908df07e04ed0a
ngobenihc <ngobenihc@yahoo.com>
2025-04-22 19:30:59 +0200

    html

---
+++ appwebsite/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+
+
+urlpatterns = [
+    #path('',views.index,name='home-page'),
+    path('',views.about,name='about-page'),
+]
--- appwebsite/views.py
+++ appwebsite/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from appwebsite.models import CompanyInfo
+
 
 # Create your views here.
+
+def about(request):
+    context ={
+        'name':'clif'
+    }
+    return render(request,'appwebsite/about.html',context)
