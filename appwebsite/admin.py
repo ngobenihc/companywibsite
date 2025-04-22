51f7940101a93b7c3d61402b0d5261c9fd64cbd8
ngobenihc <ngobenihc@yahoo.com>
2025-04-22 11:48:58 +0200

    company model

--- appwebsite/admin.py
+++ appwebsite/admin.py
@@ -3,4 +3,24 @@ from django.contrib import admin
 from appwebsite.models import CompanyInfo
 
 # Register your models here.
-admin.site.register(CompanyInfo)
+#admin.site.register(CompanyInfo)
+@admin.register(CompanyInfo)
+class CompanyInfoAdmin(admin.ModelAdmin):
+    list_display = [
+        'company_name',
+        'location',
+        'phone',
+        'email',
+        'open_hours'
+    ]
+
+
+    # def has_add_permission(self, request):
+    #     return False
+    # def has_change_permission(self, request, obj=None):
+    #     return False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
+    # readonly_fields = [
+    #     'company_name'
+    # ]
